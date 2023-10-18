3e7540d0656d3cb909390a551673b3ae1c50799d
gd388 <105911071+Sagararc@users.noreply.github.com>
2023-10-18 13:02:48 +0530

    final

--- map/urls.py
+++ map/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import show_map
+from .views import show_map,FormData,datashow,success
 
 urlpatterns = [
     # ... other patterns ...
     path('', show_map, name='show_map'),
+    path('form/' , FormData , name='form'),
+    path('data/' , datashow , name='data'),
+    path('success/' , success , name='success'),
     
 ]
--- map/views.py
+++ map/views.py
@@ -20,3 +20,44 @@ from django.shortcuts import render,HttpResponse
 def show_map(request):
     
     return render(request, 'index.html')
+
+
+from django.shortcuts import render , HttpResponse , redirect
+from .forms import Form
+from .models import FormModel
+# Create your views here.
+def FormData(request):
+    
+  
+    if request.method == "POST":
+        form = Form(request.POST)
+        if form.is_valid():
+            
+           
+            name = request.POST.get('name')
+            mobile = request.POST.get('mobile')
+            mail = request.POST.get('mail')
+            city = request.POST.get('city')
+            counts = request.POST.get('counts')
+            
+            
+            form.instance.name = name
+            form.instance.mobile = mobile
+            form.instance.mail = mail
+            form.instance.city = city
+            form.instance.counts = counts
+           
+            form.save()
+            return redirect('')
+        else:
+            print(form.errors)   
+    return render(request , 'form.html')
+
+
+def datashow(request) : 
+    formData =  FormModel.objects.all()
+    return render(request , 'data.html' , {'usr' : formData})
+
+
+def success(request):
+    return render(request , 'success.html')
